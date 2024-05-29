8c831fc96d4897f317ec0d65e579c7e60e82b2c9
atharvaaalok <atharvaaalok@gmail.com>
2024-05-29 15:42:13 +0530

    Added function that given airfoil coordinates computes its L by D ratio.

---
+++ data/compute_L_by_D.py
@@ -0,0 +1,36 @@
+import numpy as np
+import numpy.typing as npt
+from xfoil import XFoil
+from xfoil.model import Airfoil
+
+
+def compute_L_by_D(X: npt.NDArray) -> float:
+    """Given an airfoil's coordinates compute the L by D ratio.
+
+    Args:
+        X: Airfoil coordinates.
+    """
+
+    # Reshape airfoil coordinate array and get the x and y coordinates
+    X = X.reshape(-1, 2)
+    x, y = X[:, 0], X[:, 1]
+
+    # Create an airfoil object using these coordinates
+    airfoil = Airfoil(x, y)
+
+
+    # Instantiate the XFoil class
+    xf = XFoil()
+    xf.print = False
+
+    # Set the airfoil and flow properties
+    xf.airfoil = airfoil
+    xf.Re = 1e5
+
+    # Calculate aerodynamic coefficients at 0 angle of attack
+    cl, cd, cm, cp = xf.a(0)
+
+    # Calculate L by D ratio
+    L_by_D = cl / cd
+
+    return L_by_D
