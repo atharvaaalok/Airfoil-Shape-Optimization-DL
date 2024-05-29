d9561389bf420271730e1ed48708b45f4a0a59f1
atharvaaalok <atharvaaalok@gmail.com>
2024-05-29 14:51:40 +0530

    Converted MV script to a function.

--- data/generate_airfoils_MV.py
+++ data/generate_airfoils_MV.py
@@ -1,35 +1,34 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 from airfoil_noise_addition import airfoil_noise_addition
 
 
-# Load the high variance airfoils
-X_HV_all = np.load('generated_airfoils/airfoils_HV.npy')
+def generate_airfoils_MV(total_MV: int, noise_MV: float) -> None:
+    """Creates mid variance airfoils for each high variance airfoil previously generated.
+    
+    Args:
+        total_MV: The number of mid variance airfoils to be created for each high variance airfoil.
+        noise_MV: Noise level used to create mid variance airfoils from the high variance ones.
+    """
 
-# Decide the number of mid variance airfoils to be created for each high variance airfoil
-total_MV_airfoils = 2
+    # Load the high variance airfoils
+    X_HV_all = np.load('generated_airfoils/airfoils_HV.npy')
 
-# Create dummy array to hold all mid variance airfoils created from each high variance airfoil
-total_airfoils = X_HV_all.shape[0]
-total_HV_airfoils = X_HV_all.shape[1]
-num_coordinates = X_HV_all.shape[2]
-X_MV_all = np.zeros((total_airfoils, total_HV_airfoils, total_MV_airfoils, num_coordinates))
-
-
-# Decide noise level for creating mid variance airfoils
-noise_level_MV = 0.01
-
-# Generate mid variance airfoils for each high variance airfoil
-for i in range(total_airfoils):
-    for j in range(total_HV_airfoils):
-        X_HV = X_HV_all[i, j, :]
-        # Generate MV airfoils
-        for k in range(total_MV_airfoils):
-            X_new = airfoil_noise_addition(X_HV, noise_level_MV)
-            # Store the airfoil in the list of MV airfoils
-            X_MV_all[i, j, k, :] = X_new
+    total_airfoils, total_HV, num_coordinates = X_HV_all.shape
+    # Create dummy array to hold MV airfoils
+    X_MV_all = np.zeros((total_airfoils, total_HV, total_MV, num_coordinates))
 
 
-# Save the mid variance airfoils created in a single numpy file
-np.save('generated_airfoils/airfoils_MV.npy', X_MV_all)
+    # Generate mid variance airfoils for each high variance airfoil
+    for i in range(total_airfoils):
+        for j in range(total_HV):
+            X_HV = X_HV_all[i, j, :]
+            # Generate MV airfoils
+            for k in range(total_MV):
+                X_new = airfoil_noise_addition(X_HV, noise_MV)
+                # Store the airfoil in the list of MV airfoils
+                X_MV_all[i, j, k, :] = X_new
+
+
+    # Save the mid variance airfoils created in a single numpy file
+    np.save('generated_airfoils/airfoils_MV.npy', X_MV_all)
