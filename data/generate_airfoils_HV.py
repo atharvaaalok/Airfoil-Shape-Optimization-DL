5d995641cdf2979ce485a5cf0a4eaf37aaa6b8cc
atharvaaalok <atharvaaalok@gmail.com>
2024-05-29 14:51:27 +0530

    Converted HV script to a function.

--- data/generate_airfoils_HV.py
+++ data/generate_airfoils_HV.py
@@ -1,33 +1,33 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 from airfoil_noise_addition import airfoil_noise_addition
 
 
-# Load the original airfoils
-X_all = np.load('generated_airfoils/airfoils_original.npy')
+def generate_airfoils_HV(total_HV: int, noise_HV: float) -> None:
+    """Creates high variance airfoils for each original airfoil in the database.
+    
+    Args:
+        total_HV: The number of high variance airfoils to be created for each original airfoil.
+        noise_HV: Noise level used to create high variance airfoils from the original ones.
+    """
 
-# Decide the number of high variance airfoils to be created for each original airfoil
-total_HV_airfoils = 2
+    # Load the original airfoils
+    X_all = np.load('generated_airfoils/airfoils_original.npy')
 
-# Create dummy array to hold all high variance airfoils created from each original airfoil
-total_airfoils = X_all.shape[0]
-num_coordinates = X_all.shape[1]
-X_HV_all = np.zeros((total_airfoils, total_HV_airfoils, num_coordinates))
+    total_airfoils, num_coordinates = X_all.shape
+    # Create dummy array to hold HV airfoils
+    X_HV_all = np.zeros((total_airfoils, total_HV, num_coordinates))
+    
 
-
-# Decide noise level for creating high variance airfoils
-noise_level_HV = 0.1
-
-# Generate high variance airfoils for each original airfoil
-for i in range(total_airfoils):
-    X_orig = X_all[i, :]
-    # Generate HV airfoils
-    for j in range(total_HV_airfoils):
-        X_new = airfoil_noise_addition(X_orig, noise_level_HV)
-        # Store the airfoil in the list of HV airfoils
-        X_HV_all[i, j, :] = X_new
+    # Generate high variance airfoils for each original airfoil
+    for i in range(total_airfoils):
+        X_orig = X_all[i, :]
+        # Generate HV airfoils
+        for j in range(total_HV):
+            X_new = airfoil_noise_addition(X_orig, noise_HV)
+            # Store the airfoil in the list of HV airfoils
+            X_HV_all[i, j, :] = X_new
 
 
-# Save the high variance airfoils created in a single numpy file
-np.save('generated_airfoils/airfoils_HV.npy', X_HV_all)
+    # Save the high variance airfoils created in a single numpy file
+    np.save('generated_airfoils/airfoils_HV.npy', X_HV_all)
