ed61d6f6c8efda0553e224241045bd3b6983eee4
atharvaaalok <atharvaaalok@gmail.com>
2024-05-30 14:24:38 +0530

    Training on dummy data to create a model and then work on the optimization problem.

--- neural_net/net_def.py
+++ neural_net/net_def.py
@@ -4,15 +4,18 @@ from torch import nn
 
 class NeuralNetwork(nn.Module):
     
-    def __init__(self, input_dim, hidden_dim):
+    def __init__(self, input_dim, hidden_dim, layer_count):
         super().__init__()
-        self.linear1 = nn.Linear(input_dim, hidden_dim)
-        self.relu = nn.ReLU()
-        self.linear2 = nn.Linear(hidden_dim, 1)
+
+        layers = [nn.Linear(input_dim, hidden_dim), nn.ReLU()]
+        for i in range(layer_count):
+            layers.append(nn.Linear(hidden_dim, hidden_dim))
+            layers.append(nn.ReLU())
+        layers.append(nn.Linear(hidden_dim, 1))
+
+        self.linear_relu_stack = nn.Sequential(*layers)
     
 
     def forward(self, x):
-        x = self.linear1(x)
-        x = self.relu(x)
-        x = self.linear2(x)
+        x = self.linear_relu_stack(x)
         return x
--- neural_net/net_train.py
+++ neural_net/net_train.py
@@ -9,18 +9,30 @@ from utils import set_learning_rate, print_net_performance
 
 
 ## Get the data
-X_all = np.load('../data/generated_airfoils/airfoils_original.npy')
-Y_all = np.load('../data/generated_airfoils/L_by_D_original.npy').reshape(-1, 1)
-X_train = torch.from_numpy(X_all).to(torch.float32)
-Y_train = torch.from_numpy(Y_all).to(torch.float32)
-X_val = X_train
-Y_val = Y_train
+# X_all = np.load('../data/generated_airfoils/airfoils_original.npy')
+# Y_all = np.load('../data/generated_airfoils/L_by_D_original.npy').reshape(-1, 1)
+# X_train = torch.from_numpy(X_all).to(torch.float32)
+# Y_train = torch.from_numpy(Y_all).to(torch.float32)
+# X_val = X_train
+# Y_val = Y_train
 
+def f(X):
+    # For a training example - y = x1 + 2 * x2^2 + 3 * x3^.5, the fancy indexing preserves 2D shape
+    Y = X[:, 0:1] + 2 * X[:, 1:2] ** 2 + 3 * X[:, 2:3] ** 0.5
+    return Y
+
+m_train = 100000
+m_val = 100
+factor = 5
+X_train = torch.rand(m_train, 3) * factor
+Y_train = f(X_train)
+X_val = torch.rand(m_val, 3) * factor
+Y_val = f(X_val)
 
 ## Initialize the network
 # Set network properties
-input_dim, hidden_dim = 402, 5
-xfoil_net = NeuralNetwork(input_dim, hidden_dim)
+input_dim, hidden_dim, layer_count = 3, 10, 3
+xfoil_net = NeuralNetwork(input_dim, hidden_dim, layer_count)
 
 
 ## Define the loss function
@@ -35,7 +47,7 @@ optimizer = torch.optim.Adam(xfoil_net.parameters())
 epochs = 1000
 print_cost_every = 100
 B = 64
-learning_rate = 1e-6
+learning_rate = 0.01
 
 # Set learning rate
 set_learning_rate(optimizer, learning_rate)
@@ -55,11 +67,16 @@ for epoch in range(total_epochs + 1, total_epochs + epochs + 1):
     # Set network to training mode
     xfoil_net.train()
 
+    # Select a mini-batch of data
+    idx = torch.randint(0, m_train, (B,))
+    x = X_train[idx, :]
+    y = Y_train[idx, :]
+
     # Run the forward pass and calculate the predictions
-    Y_pred = xfoil_net(X_train)
+    Y_pred = xfoil_net(x)
 
     # Compute the loss
-    loss = MSELoss_fn(Y_pred, Y_train)
+    loss = MSELoss_fn(Y_pred, y)
 
     # Run the backward pass and calculate the gradients
     loss.backward()
