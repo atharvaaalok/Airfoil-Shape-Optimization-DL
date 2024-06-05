221c976f832412c6c56aecc0b304c1fbf39f9d48
John Doe <johndoe@example.com>
2024-06-05 13:32:19 +0530

    Wrote code for generation of all variants from original with HV, MV, LV noise.

--- data/generate_airfoils.py
+++ data/generate_airfoils.py
@@ -1,25 +1,41 @@
 import numpy as np
 
-from generate_airfoils_singlefile import generate_airfoils_singlefile
-from generate_airfoils_HV import generate_airfoils_HV
-from generate_airfoils_MV import generate_airfoils_MV
-from generate_airfoils_LV import generate_airfoils_LV
+from airfoil_utils import generate_airfoil_singlefile
+from airfoil_utils import generate_airfoil_variants
 
 
 # Set the number of HV, MV and LV airfoils and the noise levels used to generate them
-total_HV, noise_HV = 2, 0.1
-total_MV, noise_MV = 2, 0.01
-total_LV, noise_LV = 2, 0.001
+HV_details = {'name': 'HV', 'count': 2, 'noise': 0.1}
+MV_details = {'name': 'MV', 'count': 2, 'noise': 0.01}
+LV_details = {'name': 'LV', 'count': 2, 'noise': 0.001}
 
 
+airfoil_set = 'train'
+## 1 - original
 # Generate a single file of all original airfoils and corresponding L by D ratios
-generate_airfoils_singlefile()
+generate_airfoil_singlefile(airfoil_set)
 
-# Generate high variance airfoils for each original airfoil and their L by D ratios
-generate_airfoils_HV(total_HV, noise_HV)
+## 2 - original_LV
+# Generate low variance airfoils from the original airfoils
+airfoil_source = 'original'
+generate_airfoil_variants(airfoil_set, airfoil_source, LV_details)
 
-# Generate mid variance airfoils for each high variance airfoil and their L by D ratios
-generate_airfoils_MV(total_MV, noise_MV)
+## 3 - original_MV
+airfoil_source = 'original'
+generate_airfoil_variants(airfoil_set, airfoil_source, MV_details)
 
-# Generate low variance airfoils for each mid variance airfoil and their L by D ratios
-generate_airfoils_LV(total_LV, noise_LV)
+## 4 - original_HV
+airfoil_source = 'original'
+generate_airfoil_variants(airfoil_set, airfoil_source, HV_details)
+
+## 5 - original_MV_LV
+airfoil_source = 'original_MV'
+generate_airfoil_variants(airfoil_set, airfoil_source, LV_details)
+
+## 6 - original_HV_MV
+airfoil_source = 'original_HV'
+generate_airfoil_variants(airfoil_set, airfoil_source, MV_details)
+
+## 7 - original_HV_MV_LV
+airfoil_source = 'original_HV_MV'
+generate_airfoil_variants(airfoil_set, airfoil_source, LV_details)
