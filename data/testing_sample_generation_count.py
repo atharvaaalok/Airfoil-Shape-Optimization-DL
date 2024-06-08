764aa4de9ac3839a14094c9a7d0755af66b10d5e
atharvaaalok <atharvaaalok@gmail.com>
2024-06-08 04:48:46 +0000

    Generated samples for .7 and .5 noise. Choosing .7 noise as the number of generated samples and the time taken is not that different.

--- data/testing_sample_generation_count.py
+++ data/testing_sample_generation_count.py
@@ -23,12 +23,15 @@ fname = 'original_HV_MV_LV'
 original_HV_MV_LV = np.loadtxt(f'generated_airfoils/train/{fname}.log')
 
 
+generated_samples = 0
+
 # Print statistics for each
 print('original_LV')
 b = np.array(original_LV)
 print('average', np.mean(b))
 print('max    ', np.max(b))
 print('max 10 ', np.flip(np.sort(b))[:10])
+generated_samples += np.sum(b)
 print()
 
 print('original_MV')
@@ -36,6 +39,7 @@ b = np.array(original_MV)
 print('average', np.mean(b))
 print('max    ', np.max(b))
 print('max 10 ', np.flip(np.sort(b))[:10])
+generated_samples += np.sum(b)
 print()
 
 
@@ -44,6 +48,7 @@ b = np.array(original_HV)
 print('average', np.mean(b))
 print('max    ', np.max(b))
 print('max 10 ', np.flip(np.sort(b))[:10])
+generated_samples += np.sum(b)
 print()
 
 print('original_MV_LV')
@@ -51,6 +56,7 @@ b = np.array(original_MV_LV)
 print('average', np.mean(b))
 print('max    ', np.max(b))
 print('max 10 ', np.flip(np.sort(b))[:10])
+generated_samples += np.sum(b)
 print()
 
 print('original_HV_LV')
@@ -58,6 +64,7 @@ b = np.array(original_HV_LV)
 print('average', np.mean(b))
 print('max    ', np.max(b))
 print('max 10 ', np.flip(np.sort(b))[:10])
+generated_samples += np.sum(b)
 print()
 
 print('original_HV_MV')
@@ -65,6 +72,7 @@ b = np.array(original_HV_MV)
 print('average', np.mean(b))
 print('max    ', np.max(b))
 print('max 10 ', np.flip(np.sort(b))[:10])
+generated_samples += np.sum(b)
 print()
 
 print('original_HV_MV_LV')
@@ -72,4 +80,13 @@ b = np.array(original_HV_MV_LV)
 print('average', np.mean(b))
 print('max    ', np.max(b))
 print('max 10 ', np.flip(np.sort(b))[:10])
+generated_samples += np.sum(b)
 print()
+
+
+N = 900
+L, M, H = 5, 5, 5
+D = N * (1 + L + M + H + M * L + H * L + H * M + H * M * L)
+print('Actual samples   :', D)
+generated_samples += N
+print('Generated samples: ', generated_samples)
