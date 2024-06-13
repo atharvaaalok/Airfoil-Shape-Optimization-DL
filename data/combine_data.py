a13f71657ce4e66bb12315488f5650dbfbad90e3
atharvaaalok <atharvaaalok@gmail.com>
2024-06-13 04:43:10 +0000

    Added script that combines all the airfoil data arrays.

---
+++ data/combine_data.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+dataset_count_list = [i for i in range(1, 11)]
+airfoil_set = 'train'
+
+# Initialize lists to hold each dataset's combined data arrays
+P_combine_all_dataset = []
+L_by_D_combine_all_dataset = []
+
+
+for dataset_count in dataset_count_list:
+    dir_name = f'generated_airfoils/{airfoil_set}/{dataset_count}'
+
+    P_combine = []
+    L_by_D_combine = []
+
+    # 1. original
+    data = np.load(dir_name + 'original.npz')
+    P_combine.append(data['P'])
+    L_by_D_combine.append(data['L_by_D'])
+
+    # 2. original_LV
+    data = np.load(dir_name + 'original_LV.npz')
+    P_combine.append(data['P'])
+    L_by_D_combine.append(data['L_by_D'])
+
+    # 3. original_MV
+    data = np.load(dir_name + 'original_MV.npz')
+    P_combine.append(data['P'])
+    L_by_D_combine.append(data['L_by_D'])
+
+    # 4. original_HV
+    data = np.load(dir_name + 'original_HV.npz')
+    P_combine.append(data['P'])
+    L_by_D_combine.append(data['L_by_D'])
+
+    # 5. original_MV_LV
+    data = np.load(dir_name + 'original_MV_LV.npz')
+    P_combine.append(data['P'])
+    L_by_D_combine.append(data['L_by_D'])
+
+    # 6. original_HV_LV
+    data = np.load(dir_name + 'original_HV_LV.npz')
+    P_combine.append(data['P'])
+    L_by_D_combine.append(data['L_by_D'])
+
+    # 7. original_HV_MV
+    data = np.load(dir_name + 'original_HV_MV.npz')
+    P_combine.append(data['P'])
+    L_by_D_combine.append(data['L_by_D'])
+
+    # 8. original_HV_MV_LV
+    data = np.load(dir_name + 'original_HV_MV_LV.npz')
+    P_combine.append(data['P'])
+    L_by_D_combine.append(data['L_by_D'])
+
+
+    # Stack all the data into a single array
+    P_combine = np.vstack(P_combine)
+    L_by_D_combine = np.hstack(L_by_D_combine)
+
+    # Store in a combined data file for each dataset
+    np.savez(dir_name + 'combined_data.npz', P = P_combine, L_by_D = L_by_D_combine)
+    
+
+    # Append data to all dataset list
+    P_combine_all_dataset.append(P_combine)
+    L_by_D_combine_all_dataset.append(L_by_D_combine)
+
+
+# Stack all the datasets combined data into a single giant array
+P_combine_all_dataset = np.vstack(P_combine_all_dataset)
+L_by_D_combine_all_dataset = np.hstack(L_by_D_combine_all_dataset)
+
+# Store in a single data file
+np.savez(f'generated_airfoils/{airfoil_set}/' + 'airfoil_data.npz',
+         P = P_combine_all_dataset,
+         L_by_D = L_by_D_combine_all_dataset)
