c2490de011e9788cf095dae29819455a6b7859e8
atharvaaalok <atharvaaalok@gmail.com>
2024-06-13 21:53:03 +0530

    Added pytorch documentation style workflow for neural network training.

---
+++ neural_net/net_data.py
@@ -0,0 +1,23 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class AirfoilDataset(Dataset):
+    def __init__(self, file_path):
+        # Load the data from the combined data file
+        data = np.load(file_path)
+        self.P = data['P']
+        self.L_by_D = data['L_by_D']
+    
+
+    def __len__(self):
+        return self.P.shape[0]
+
+
+    def __getitem__(self, idx):
+        # Return the sample at the given index
+        x = self.P[idx]
+        y = self.L_by_D[idx]
+        y = y.reshape(1, )
+        return torch.tensor(x, dtype = torch.float32),  torch.tensor(y, dtype = torch.float32)
