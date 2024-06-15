f522061ab184eb92c28dad2f670025aadd9641da
atharvaaalok <atharvaaalok@gmail.com>
2024-06-15 13:50:22 +0530

    Wrote new code for direct data loading and using that for training.

---
+++ neural_net/net_train.py
@@ -0,0 +1,94 @@
+import os.path
+
+import numpy as np
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+from net_def import NeuralNetwork
+from net_data import AirfoilDataset
+from utils import set_learning_rate, train_loop, dev_loop, red, cyan, color_end
+
+
+## Get the data
+train_filepath = '../data/generated_airfoils/train/airfoil_data.npz'
+dev_filepath = '../data/generated_airfoils/dev/airfoil_data.npz'
+
+data_train = np.load(train_filepath)
+data_dev = np.load(train_filepath)
+
+X_train = torch.from_numpy(data_train['P']).to(torch.float32)
+Y_train = torch.from_numpy(data_train['L_by_D']).to(torch.float32).reshape(-1, 1)
+X_val = torch.from_numpy(data_dev['P']).to(torch.float32)
+Y_val = torch.from_numpy(data_dev['L_by_D']).to(torch.float32).reshape(-1, 1)
+
+
+## Initialize the network
+# Set network properties
+input_dim, hidden_dim, layer_count = 24, 30, 4
+xfoil_net = NeuralNetwork(input_dim, hidden_dim, layer_count)
+
+
+## Define the loss function
+MSELoss_fn = nn.MSELoss()
+
+
+## Define an optimizer
+learning_rate = 0.01
+optimizer = torch.optim.Adam(xfoil_net.parameters(), lr = learning_rate)
+
+
+## Train the network
+# Set the training properties
+epochs = 100000
+print_cost_every = 10000
+B = 900
+
+
+# # Load saved model if available
+# if os.path.exists('checkpoints/latest.pth'):
+#     checkpoint = torch.load('checkpoints/latest.pth')
+#     xfoil_net.load_state_dict(checkpoint['model'])
+#     optimizer.load_state_dict(checkpoint['optimizer'])
+#     total_epochs = checkpoint['total_epochs']
+# else:
+#     total_epochs = 0
+total_epochs = 0
+
+# Train the network
+for epoch in range(total_epochs + 1, total_epochs + epochs + 1):
+
+    verbose = True if epoch % print_cost_every == 0 or epoch == total_epochs + 1 else False
+    save = verbose
+
+    if verbose:
+        print(f'{red}Epoch {epoch}{color_end}\n' + 40 * '-')
+    
+
+    # Run the training loop
+    train_loop(X_train, Y_train, B, xfoil_net, MSELoss_fn, optimizer, verbose = False)
+
+    # Run the validation loop
+    J_val = dev_loop(X_val, Y_val, B, xfoil_net, MSELoss_fn, verbose)
+
+
+    if save:
+        # Create checkpoint and save the model
+        checkpoint = {
+            'total_epochs': epoch,
+            'model': xfoil_net.state_dict(),
+            'optimizer': optimizer.state_dict(),
+        }
+        # Save the model twice: once on its own and once in the latest model file
+        torch.save(checkpoint, f'checkpoints/xfoil_net_Epoch_{epoch}_Jval_{J_val:.3e}.pth')
+        torch.save(checkpoint, f'checkpoints/latest.pth')
+
+
+print('Finished Training!')
+
+
+# Evaluate the model
+xfoil_net.eval()
+X, Y = X_val, Y_val
+Y_pred = xfoil_net(X)
+print(torch.hstack([Y, Y_pred]))
--- neural_net/utils.py
+++ neural_net/utils.py
@@ -47,6 +47,104 @@ def print_net_performance(epochs: int, epoch: int, J_train: float, J_val: float 
     print(print_performance)
 
 
+
+def train_loop(X_train, Y_train, B, model, loss_fn, optimizer, verbose = False):
+
+    N = X_train.shape[0]
+    # Ensure B perfectly divides N
+    assert N % B == 0, "B should perfectly divide N"
+
+    # Generate a random permutation of indices from 0 to N-1
+    perm = torch.randperm(N)
+    # Index the tensor with the permutation to shuffle the rows
+    X_train = X_train[perm]
+    Y_train = Y_train[perm]
+
+    # Figure out the number of batches and the batch indices
+    num_batches = N // B
+
+    idx = torch.arange(N)
+    idx = idx.view(num_batches, B)
+    # Convert each row of the reshaped tensor to a separate tensor and store them in a list
+    batch_indices = [idx[i] for i in range(num_batches)]
+
+
+    num_digits = len(str(num_batches))
+
+    # Set the model to training mode
+    model.train()
+
+    # Run the training loop
+    for batch in range(num_batches):
+        X, Y = X_train[batch_indices[batch], :], Y_train[batch_indices[batch], :]
+
+        # Run the forward pass
+        Y_pred = model(X)
+
+        # Compute the loss
+        loss = loss_fn(Y_pred, Y)
+
+        # Run the backward pass and calculate the gradients
+        loss.backward()
+
+        # Take an update step and then zero out the gradients
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if verbose:
+            if batch % (num_batches // 5) == 0:
+                loss = loss.item()
+                print(f'{cyan}Train Loss:{color_end} [{batch + 1:{num_digits}}/{num_batches}] {loss:20.6f}')
+    if verbose:
+        print()
+
+
+def dev_loop(X_val, Y_val, B, model, loss_fn, verbose = False):
+    N = X_val.shape[0]
+    # Ensure B perfectly divides N
+    assert N % B == 0, "B should perfectly divide N"
+
+    # Generate a random permutation of indices from 0 to N-1
+    perm = torch.randperm(N)
+    # Index the tensor with the permutation to shuffle the rows
+    X_val = X_val[perm]
+    Y_val = Y_val[perm]
+
+    # Figure out the number of batches and the batch indices
+    num_batches = N // B
+
+    idx = torch.arange(N)
+    idx = idx.view(num_batches, B)
+    # Convert each row of the reshaped tensor to a separate tensor and store them in a list
+    batch_indices = [idx[i] for i in range(num_batches)]
+
+
+    num_digits = len(str(num_batches))
+    test_loss = 0
+
+    # Set the model to evaluation mode
+    model.eval()
+
+    # Evaluate the model with torch.no_grad() to ensure no gradients are computed
+    with torch.no_grad():
+        for batch in range(num_batches):
+            X, Y = X_val[batch_indices[batch], :], Y_val[batch_indices[batch], :]
+
+            # Run the forward pass
+            Y_pred = model(X)
+
+            # Compute the loss
+            loss = loss_fn(Y_pred, Y)
+            test_loss += loss.item()
+    
+    test_loss = test_loss / num_batches
+    if verbose:
+        print(f'{cyan}Valid Loss:{color_end} {test_loss:20.6f}')
+        print()
+    
+    return test_loss
+
+
 def train_loop_old(dataloader, model, loss_fn, optimizer, verbose = False):
     num_batches = len(dataloader)
     num_digits = len(str(num_batches))
