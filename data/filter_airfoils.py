8e9c6dc26ce686ab664b93cf0e031bc1c74838df
atharvaaalok <atharvaaalok@gmail.com>
2024-06-17 11:12:31 +0530

    Added code to filter the generated airfoils to only include those with L by D ratios that not unphysically large like 1e10.

---
+++ data/filter_airfoils.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+airfoil_set = 'train'
+data = np.load(f'generated_airfoils/{airfoil_set}/airfoil_data.npz')
+
+P = data['P']
+L_by_D = data['L_by_D']
+
+# Sort the airfoils by L by D ratio
+idx = np.argsort(L_by_D)
+L_by_D = L_by_D[idx]
+P = P[idx, :]
+
+# Remove the n_largest and the n_smallest airfoils
+n_largest = 5000
+n_smallest = 3000
+P = P[n_smallest: -n_largest, :]
+L_by_D = L_by_D[n_smallest: -n_largest]
+
+
+# Shuffle the airfoils instead of the current sorted order
+idx = np.arange(P.shape[0])
+np.random.shuffle(idx)
+P = P[idx, :]
+L_by_d = L_by_D[idx]
+
+
+# Save the filter aifoils
+np.savez(f'generated_airfoils/{airfoil_set}/airfoil_data.npz', P = P, L_by_D = L_by_D)
