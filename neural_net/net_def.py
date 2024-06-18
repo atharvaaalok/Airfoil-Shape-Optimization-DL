3bc721678ac1317843000bd0eb4a137ea777c42a
atharvaaalok <atharvaaalok@gmail.com>
2024-06-18 12:04:51 +0530

    Removed the now redundant original network definition that is stored where the model weights are stored.

--- neural_net/net_def.py
+++ neural_net/net_def.py
@@ -71,24 +71,3 @@ class ResidualBlock(nn.Module):
         x = self.bn2(x)
         x = x + residual
         return x
-
-
-
-class Model_Original_TrainError0(nn.Module):
-    
-    def __init__(self, input_dim, hidden_dim, layer_count):
-        super().__init__()
-
-        layers = [nn.Linear(input_dim, hidden_dim), nn.ReLU(), nn.BatchNorm1d(hidden_dim)]
-        for i in range(layer_count):
-            layers.append(nn.Linear(hidden_dim, hidden_dim))
-            layers.append(nn.ReLU())
-            layers.append(nn.BatchNorm1d(hidden_dim))
-        layers.append(nn.Linear(hidden_dim, 1))
-
-        self.linear_relu_stack = nn.Sequential(*layers)
-    
-
-    def forward(self, x):
-        x = self.linear_relu_stack(x)
-        return x
