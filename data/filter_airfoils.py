ff14f415377ae4c9c510ed454ba6900df450c8c3
atharvaaalok <atharvaaalok@gmail.com>
2024-06-27 14:31:21 +0530

    Filtered airfoils to exclude unreasonable L by D ratios for the airfoils that do not include the HV->MV->LV airfoils.

--- data/filter_airfoils.py
+++ data/filter_airfoils.py
@@ -13,8 +13,8 @@ L_by_D = L_by_D[idx]
 P = P[idx, :]
 
 # Remove the n_largest and the n_smallest airfoils
-n_largest = 5000
-n_smallest = 3000
+n_largest = 25000
+n_smallest = 25000
 P = P[n_smallest: -n_largest, :]
 L_by_D = L_by_D[n_smallest: -n_largest]
 
@@ -23,8 +23,8 @@ L_by_D = L_by_D[n_smallest: -n_largest]
 idx = np.arange(P.shape[0])
 np.random.shuffle(idx)
 P = P[idx, :]
-L_by_d = L_by_D[idx]
+L_by_D = L_by_D[idx]
 
 
 # Save the filter aifoils
-np.savez(f'generated_airfoils/{airfoil_set}/airfoil_data.npz', P = P, L_by_D = L_by_D)
+np.savez(f'generated_airfoils/{airfoil_set}/airfoil_data_filtered.npz', P = P, L_by_D = L_by_D)
